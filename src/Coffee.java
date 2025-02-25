dfe887246a8c59c30664bab434abb73076f7d3a8
Dmitry Sayfulin <sayfulindmitry@gmail.com>
2025-02-25 19:25:57 +0300

    add hw1

---
+++ src/Coffee.java
@@ -0,0 +1,35 @@
+/**
+ * Класс Coffee
+ *
+ * Поля (примерная идея):
+ *   - private String name;       // Название кофе
+ *   - private double basePrice;  // Базовая цена
+ *
+ * Геттеры/сеттеры:
+ *   - getName(), setName(...)
+ *   - getBasePrice(), setBasePrice(...)
+ *
+ * Методы (примерные идеи):
+ *   - calculatePriceWithSugar(Sugar sugar): double
+ *       (может возвращать basePrice + некая надбавка за сахар, или просто basePrice, если надбавки нет)
+ *
+ *   - toString() (не обязательно, но может упростить вывод информации)
+ */
+public class Coffee {
+    // Напишите код здесь (объявление полей)
+
+    // Напишите код здесь (конструктор/конструкторы)
+
+    // Напишите код здесь (геттеры/сеттеры)
+
+    // public double calculatePriceWithSugar(Sugar sugar) {
+    //     // Напишите код здесь:
+    //     // Пример:
+    //     // double finalPrice = basePrice + sugar.getSugarPrice();
+    //     // return finalPrice;
+    // }
+
+    // public String toString() {
+    //     // Напишите код здесь (опционально)
+    // }
+}
